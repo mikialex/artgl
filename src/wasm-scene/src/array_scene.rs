c2414481515414ddd670075a57b1f63cff0c53ed
mikialex <18516340862@163.com>
2019-04-22 00:39:08 +0800

    dev

--- src/wasm-scene/src/array_scene.rs
+++ src/wasm-scene/src/array_scene.rs
@@ -2,43 +2,44 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct ArrayScene{
-  localTransformArray: Vec<f32>,
-  localPositionArray: Vec<f32>,
-  worldTransformArray: Vec<f32>,
-  worldAABBArray: Vec<f32>,
-  worldBSphereArray: Vec<f32>,
+  local_transform_array: Vec<f32>,
+  local_position_array: Vec<f32>,
 
-  emptyArray: Vec<u8>,
-  emptyListArray: Vec<u16>,
-  emptyCount: u16,
+  world_transform_array: Vec<f32>,
+  world_aabb_array: Vec<f32>,
+  world_bsphere_array: Vec<f32>,
 
-  nodesIndexs: Vec<u16>,
+  empty_array: Vec<u8>,
+  empty_list_array: Vec<u16>,
+  empty_count: u16,
+
+  nodes_indexs: Vec<u16>,
 }
 
 const default_node_capacity: usize = 100000;
-const localTransformArrayStride: usize = 12;
-const localPositionArrayStride: usize = 3;
-const worldTransformArrayStride: usize = 12;
-const worldAABBArrayStride: usize = 6;
-const worldBSphereArrayStride: usize = 4;
-const nodeIndexStride: usize = 4;
+const LOCAL_TRANSFORM_ARRAY_STRIDE: usize = 12;
+const LOCAL_POSITION_ARRAY_STRIDE: usize = 3;
+const WORLD_AABB_ARRAY_STRIDE: usize = 12;
+const world_aabb_array_stride: usize = 6;
+const world_bsphere_array_stride: usize = 4;
+const node_index_stride: usize = 4;
 
 
 #[wasm_bindgen]
 impl ArrayScene {
   pub fn new() -> ArrayScene {
     ArrayScene {
-      localTransformArray: Vec::with_capacity(100),
-      localPositionArray: Vec::with_capacity(100),
-      worldTransformArray: Vec::with_capacity(100),
-      worldAABBArray: Vec::with_capacity(100),
-      worldBSphereArray: Vec::with_capacity(100),
+      local_transform_array: Vec::with_capacity(100),
+      local_position_array: Vec::with_capacity(100),
+      world_transform_array: Vec::with_capacity(100),
+      world_aabb_array: Vec::with_capacity(100),
+      world_bsphere_array: Vec::with_capacity(100),
 
-      emptyArray: Vec::with_capacity(100),
-      emptyListArray: Vec::with_capacity(100),
-      emptyCount: 0,
+      empty_array: Vec::with_capacity(100),
+      empty_list_array: Vec::with_capacity(100),
+      empty_count: 0,
 
-      nodesIndexs: Vec::with_capacity(100),
+      nodes_indexs: Vec::with_capacity(100),
     }
   }
 
@@ -53,24 +54,34 @@ impl ArrayScene {
   }
 
   fn traverse_from(&self, node_index: u16, 
-  visitor: &Fn(u16) -> () 
+  visitor: &Fn(u16, &mut ArrayScene) -> () 
   ){
     
   }
 
   fn update_hirerachy(&self){
-    self.traverse_from(0, self.update_hirerachy_visitor)
+    self.traverse_from(0, &update_hirerachy_visitor)
   }
 
-  fn update_hirerachy_visitor(){
+}
 
-  }
+fn update_hirerachy_visitor(index: u16, scene: &mut ArrayScene){
+  update_localmatrix(index);
+  update_worldmatrix_by_parent(index);
+}
 
-  fn update_worldmatrix_by_parent(node_index: u16){
+use na::{Matrix4};
 
-  }
+fn update_worldmatrix_by_parent(index: u16){
+  let local = Matrix4::new(
+    1.0, 1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0, 1.0,
+  );
+  
+}
 
-  fn update_localmatrix(node_index: u16){
+fn update_localmatrix(index: u16){
 
-  }
 }
--- src/wasm-scene/src/lib.rs
+++ src/wasm-scene/src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 mod array_scene;
 
 use wasm_bindgen::prelude::*;
+extern crate nalgebra as na;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
