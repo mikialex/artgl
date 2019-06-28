3093512d3fb79f711fe7ac117aade8ac74633db4
MikiAlex <18516340862@163.com>
2019-06-28 15:49:00 +0800

    add test

--- src/math/test/vector3.test.ts
+++ src/math/test/vector3.test.ts
@@ -1,4 +1,57 @@
+import { Vector3 } from "../vector3";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+
+test('vector default value 0, 0, 0', () => {
+  const v = new Vector3();
+  expect(v.x).toBe(0); 
+  expect(v.y).toBe(0); 
+  expect(v.z).toBe(0); 
+});
+
+test('vector constructor', () => {
+  const v = new Vector3(1, 3, 4);
+  expect(v.x).toBe(1); 
+  expect(v.y).toBe(3); 
+  expect(v.z).toBe(4); 
+});
+
+test('vector set', () => {
+  const v = new Vector3(1, 3, 4);
+  v.set(5,6,7)
+  expect(v.x).toBe(5); 
+  expect(v.y).toBe(6); 
+  expect(v.z).toBe(7); 
+});
+
+test('vector copy', () => {
+  const v1 = new Vector3(1, 3, 4);
+  const v2 = new Vector3(0, -1, -2);
+  v1.copy(v2);
+  expect(v1.x).toBe(0); 
+  expect(v1.y).toBe(-1); 
+  expect(v1.z).toBe(-2); 
+});
+
+test('vector equals', () => {
+  const v1 = new Vector3(1, 3, 4);
+  const v2 = new Vector3(1, 3, 4);
+  expect(v1.equals(v2)).toBe(true);
+});
+
+test('vector add', () => {
+  const v1 = new Vector3(1, 3, 4);
+  const v2 = new Vector3(1, 2, 4);
+  v1.add(v2);
+  expect(v1.x).toBe(2); 
+  expect(v1.y).toBe(5); 
+  expect(v1.z).toBe(8); 
+});
+
+test('vector sub', () => {
+  const v1 = new Vector3(1, 3, 4);
+  const v2 = new Vector3(1, 2, 4);
+  v1.sub(v2);
+  expect(v1.x).toBe(0); 
+  expect(v1.y).toBe(1); 
+  expect(v1.z).toBe(0); 
 });
