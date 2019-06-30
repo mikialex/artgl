49b2347cd438fe84d701dd0457b943b466d93e53
MikiAlex <18516340862@163.com>
2019-06-30 17:18:58 +0800

    update parser

--- src/shader-graph/parser/shader-function-meta.ts
+++ src/shader-graph/parser/shader-function-meta.ts
@@ -1,24 +1,39 @@
 import { Parjs } from '../../../node_modules/parjs/index'; // idont know why
+import { GLDataType } from "../../webgl/shader-util";
 
-const floatType = Parjs.string("float");
-const Vec2Type = Parjs.string("vec2");
-const Vec3Type = Parjs.string("vec3");
-const Vec4Type = Parjs.string("vec4");
+export const floatType = Parjs.string("float").map(_value => GLDataType.float);
+export const Vec2Type = Parjs.string("vec2").map(_value => GLDataType.floatVec2);
+export const Vec3Type = Parjs.string("vec3").map(_value => GLDataType.floatVec3);
+export const Vec4Type = Parjs.string("vec4").map(_value => GLDataType.floatVec4);
 
-const dataType = floatType.or(Vec2Type).or(Vec3Type).or(Vec4Type)
+export const dataType = floatType.or(Vec2Type).or(Vec3Type).or(Vec4Type)
 
-const varName = Parjs.letter.then(Parjs.digit.or(Parjs.letter).many()).str;
+const extendLetter =  Parjs.letter.or("_")
+export const varName = extendLetter.then(Parjs.digit.or(extendLetter).many()).str;
 
-const oneParam = Parjs.seq(Parjs.space, dataType, Parjs.spaces1, varName, Parjs.space, );
+export const oneParam = Parjs.seq(Parjs.whitespaces.q, dataType, Parjs.spaces1.q, varName, Parjs.whitespaces.q).map(
+  value => {
+    return {
+      name: value[1],
+      type: value[0]
+    }
+  }
+)
 
-const paramList = oneParam.manySepBy(",");
+export const paramList = oneParam.manySepBy(",").between("(", ")");
+// const paramList = oneParam.manySepBy(",").maybe(Parjs.string(",").q).then(Parjs.whitespaces.q);
+// const paramList = Parjs.whitespaces.maybe(oneParam).manySepBy(",")
 
-const functionHead = Parjs.seq(Parjs.space, dataType, Parjs.spaces1, varName, Parjs.space, paramList, Parjs.space);
+const functionBody = Parjs.string("{").then(Parjs.anyChar.manyTill(Parjs.string("}")))
+export const functionp = Parjs.seq(
+  Parjs.whitespaces.q,
+  dataType,
+  Parjs.spaces1.q,
+  varName,
+  Parjs.whitespaces.q,
+  paramList,
+  Parjs.whitespaces.q,
+  functionBody.q,
+  Parjs.whitespaces.q,
+);
 
-const functionBody = Parjs.anyChar.many().between("{", "}")
-
-const functionp = functionHead.then(functionBody)
-
-export function parseGLSLFunctionMeta(shaderString: string) {
-  
-}
