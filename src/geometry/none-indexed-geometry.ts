8fa019f5f6513dc1c78e5471fc3f67324596f004
mikialex <18516340862@163.com>
2019-09-13 01:31:16 +0800

    adjust

--- src/geometry/none-indexed-geometry.ts
+++ src/geometry/none-indexed-geometry.ts
@@ -1,27 +1,15 @@
 import { Geometry } from "../core/geometry";
 
-export class NoneIndexedGeometry extends Geometry{
-  updateAABBBox(): void {
-    throw new Error("Method not implemented.");
-  }  updateBoundingSphere(): void {
-    throw new Error("Method not implemented.");
-  }
+// export class NoneIndexedGeometry extends Geometry{
+//   updateAABBBox(): void {
+//     throw new Error("Method not implemented.");
+//   }  updateBoundingSphere(): void {
+//     throw new Error("Method not implemented.");
+//   }
 
-  shape(): void {
-    throw new Error("Method not implemented.");
-  }
-
-  foreachFace(visitor: (face: import("../math/entity/face3").Face3) => , range?: import("../artgl").RenderRange | undefined) {
-    throw new Error("Method not implemented.");
-  }
-
-  foreachLineSegment(visitor: (face: import("../math/entity/line3").Line3) => , range?: import("../artgl").RenderRange | undefined) {
-    throw new Error("Method not implemented.");
-  }
-  
-  foreachVertex(visitor: (face: import("../math").Vector3) => , range?: import("../artgl").RenderRange | undefined) {
-    throw new Error("Method not implemented.");
-  }
+//   shape(): void {
+//     throw new Error("Method not implemented.");
+//   }
 
 
-}
+// }
--- src/webgl/texture-util.ts
+++ src/webgl/texture-util.ts
@@ -2,32 +2,32 @@ import { MathUtil } from "../math";
 
 type ImageLike = HTMLImageElement | HTMLCanvasElement | ImageBitmap;
 
-export function resizeImage(image: ImageLike, needsPowerOfTwo, canvas: HTMLCanvasElement, maxSize): ImageData {
+// export function resizeImage(image: ImageLike, needsPowerOfTwo, canvas: HTMLCanvasElement, maxSize): ImageData {
 
-  let scale = 1;
+//   let scale = 1;
 
-  // handle case if texture exceeds max size
-  if (image.width > maxSize || image.height > maxSize) {
-    scale = maxSize / Math.max(image.width, image.height);
-  }
+//   // handle case if texture exceeds max size
+//   if (image.width > maxSize || image.height > maxSize) {
+//     scale = maxSize / Math.max(image.width, image.height);
+//   }
 
-  // only perform resize if necessary
-  if (scale < 1 || needsPowerOfTwo === true) {
+//   // only perform resize if necessary
+//   if (scale < 1 || needsPowerOfTwo === true) {
 
-    const width = MathUtil.floorPowerOfTwo(scale * image.width);
-    const height = MathUtil.floorPowerOfTwo(scale * image.height);
+//     const width = MathUtil.floorPowerOfTwo(scale * image.width);
+//     const height = MathUtil.floorPowerOfTwo(scale * image.height);
 
-    canvas.width = width;
-    canvas.height = height;
+//     canvas.width = width;
+//     canvas.height = height;
 
-    var context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0, width, height);
+//     var context = canvas.getContext('2d');
+//     context.drawImage(image, 0, 0, width, height);
 
-    console.warn('Texture has been resized from (' + image.width + 'x' + image.height + ') to (' + width + 'x' + height + ').');
+//     console.warn('Texture has been resized from (' + image.width + 'x' + image.height + ') to (' + width + 'x' + height + ').');
 
-    return context.getImageData(10, 10, 50, 50);;
+//     return context.getImageData(10, 10, 50, 50);;
 
-  }
+//   }
 
 
-}
+// }
