8127c80684a8dc22496bbc760fc90cb456fa6099
mikialex <18516340862@163.com>
2019-10-22 00:09:16 +0800

    dev

--- src/interact/first-person-controller.ts
+++ src/interact/first-person-controller.ts
@@ -1,10 +1,12 @@
 import { Controller } from "./controller";
 import { Interactor } from "./interactor";
 import { SceneNode } from "../scene/scene-node";
-import { Vector3 } from "../math";
+import { Vector3, MathUtil } from "../math";
 import { Spherical } from "../math/spherical";
 
-export class FirstPersonController extends Controller{
+const targetPosition = new Vector3();
+
+export class FirstPersonController extends Controller {
   constructor(object: SceneNode) {
     super();
     this.object = object
@@ -12,44 +14,144 @@ export class FirstPersonController extends Controller{
 
   object: SceneNode;
 
-	movementSpeed = 1.0;
-	lookSpeed = 0.005;
+  movementSpeed = 1.0;
+  lookSpeed = 0.005;
 
-	lookVertical = true;
-	autoForward = false;
+  lookVertical = true;
+  autoForward = false;
 
-	activeLook = true;
+  activeLook = true;
 
-	heightSpeed = false;
-	heightCoef = 1.0;
-	heightMin = 0.0;
-	heightMax = 1.0;
+  heightSpeed = false;
+  heightCoef = 1.0;
+  heightMin = 0.0;
+  heightMax = 1.0;
 
-	constrainVertical = false;
-	verticalMin = 0;
-	verticalMax = Math.PI;
+  constrainVertical = false;
+  verticalMin = 0;
+  verticalMax = Math.PI;
 
   mouseDragOn = false;
-  
+
   private autoSpeedFactor = 0.0;
 
-	private mouseX = 0;
-	private mouseY = 0;
+  private mouseX = 0;
+  private mouseY = 0;
 
-	private moveForward = false;
-	private moveBackward = false;
-	private moveLeft = false;
-	private moveRight = false;
+  private moveForward = false;
+  private moveBackward = false;
+  private moveLeft = false;
+  private moveRight = false;
+  private moveUp = false;
+  private moveDown = false;
 
-	private viewHalfX = 0;
+  private viewHalfX = 0;
   private viewHalfY = 0;
-  
-  private lat = 0;
-	private lon = 0;
 
-	private lookDirection = new Vector3();
-	private spherical = new Spherical();
-	private target = new Vector3();
+  private lat = 0;
+  private lon = 0;
+
+  private lookDirection = new Vector3();
+  private spherical = new Spherical();
+  private target = new Vector3();
+
+  onKeyDown(event: KeyboardEvent) {
+    switch (event.keyCode) {
+      case 38: /*up*/
+      case 87: /*W*/ this.moveForward = true; break;
+
+      case 37: /*left*/
+      case 65: /*A*/ this.moveLeft = true; break;
+
+      case 40: /*down*/
+      case 83: /*S*/ this.moveBackward = true; break;
+
+      case 39: /*right*/
+      case 68: /*D*/ this.moveRight = true; break;
+
+      case 82: /*R*/ this.moveUp = true; break;
+      case 70: /*F*/ this.moveDown = true; break;
+
+    }
+  }
+
+  onKeyUp(event) {
+    switch (event.keyCode) {
+
+      case 38: /*up*/
+      case 87: /*W*/ this.moveForward = false; break;
+
+      case 37: /*left*/
+      case 65: /*A*/ this.moveLeft = false; break;
+
+      case 40: /*down*/
+      case 83: /*S*/ this.moveBackward = false; break;
+
+      case 39: /*right*/
+      case 68: /*D*/ this.moveRight = false; break;
+
+      case 82: /*R*/ this.moveUp = false; break;
+      case 70: /*F*/ this.moveDown = false; break;
+
+    }
+  }
+
+
+  update(delta) {
+
+    if (this.heightSpeed) {
+      var y = MathUtil.clamp(this.object.transform.position.y, this.heightMin, this.heightMax);
+      var heightDelta = y - this.heightMin;
+      this.autoSpeedFactor = delta * (heightDelta * this.heightCoef);
+    } else {
+      this.autoSpeedFactor = 0.0;
+    }
+
+    const actualMoveSpeed = delta * this.movementSpeed;
+
+    if (this.moveForward || (this.autoForward && !this.moveBackward)) {
+      this.object.translateZ(- (actualMoveSpeed + this.autoSpeedFactor));
+    }
+    if (this.moveBackward) this.object.translateZ(actualMoveSpeed);
+
+    if (this.moveLeft) this.object.translateX(- actualMoveSpeed);
+    if (this.moveRight) this.object.translateX(actualMoveSpeed);
+
+    if (this.moveUp) this.object.translateY(actualMoveSpeed);
+    if (this.moveDown) this.object.translateY(- actualMoveSpeed);
+
+    var actualLookSpeed = delta * this.lookSpeed;
+
+    if (!this.activeLook) {
+      actualLookSpeed = 0;
+    }
+
+    var verticalLookRatio = 1;
+
+    if (this.constrainVertical) {
+      verticalLookRatio = Math.PI / (this.verticalMax - this.verticalMin);
+    }
+
+    this.lon -= this.mouseX * actualLookSpeed;
+    if (this.lookVertical) this.lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
+
+    this.lat = Math.max(- 85, Math.min(85, this.lat));
+
+    var phi = MathUtil.degToRad(90 - this.lat);
+    var theta = MathUtil.degToRad(this.lon);
+
+    if (this.constrainVertical) {
+      phi = MathUtil.mapLinear(phi, 0, Math.PI, this.verticalMin, this.verticalMax);
+    }
+
+    var position = this.object.transform.position;
+
+    targetPosition.setFromSphericalCoords(1, phi, theta).add(position);
+
+    this.object.transform.lookAt(targetPosition);
+
+  };
+
 
 
   registerInteractor(interactor: Interactor) {
@@ -57,7 +159,7 @@ export class FirstPersonController extends Controller{
       this.interactor.unbindControllerAllListener(this);
     }
     this.interactor = interactor;
-    
+
     // this.interactor.bindLeftMouseMove(this, this.rotate);
     // this.interactor.bindRightMouseMove(this, this.move);
     // this.interactor.bindMouseWheel(this, this.zoom);
@@ -154,54 +256,6 @@ export class FirstPersonController extends Controller{
 
 // 	};
 
-// 	this.onKeyDown = function ( event ) {
-
-// 		//event.preventDefault();
-
-// 		switch ( event.keyCode ) {
-
-// 			case 38: /*up*/
-// 			case 87: /*W*/ this.moveForward = true; break;
-
-// 			case 37: /*left*/
-// 			case 65: /*A*/ this.moveLeft = true; break;
-
-// 			case 40: /*down*/
-// 			case 83: /*S*/ this.moveBackward = true; break;
-
-// 			case 39: /*right*/
-// 			case 68: /*D*/ this.moveRight = true; break;
-
-// 			case 82: /*R*/ this.moveUp = true; break;
-// 			case 70: /*F*/ this.moveDown = true; break;
-
-// 		}
-
-// 	};
-
-// 	this.onKeyUp = function ( event ) {
-
-// 		switch ( event.keyCode ) {
-
-// 			case 38: /*up*/
-// 			case 87: /*W*/ this.moveForward = false; break;
-
-// 			case 37: /*left*/
-// 			case 65: /*A*/ this.moveLeft = false; break;
-
-// 			case 40: /*down*/
-// 			case 83: /*S*/ this.moveBackward = false; break;
-
-// 			case 39: /*right*/
-// 			case 68: /*D*/ this.moveRight = false; break;
-
-// 			case 82: /*R*/ this.moveUp = false; break;
-// 			case 70: /*F*/ this.moveDown = false; break;
-
-// 		}
-
-// 	};
-
 // 	this.lookAt = function ( x, y, z ) {
 
 // 		if ( x.isVector3 ) {
@@ -222,77 +276,6 @@ export class FirstPersonController extends Controller{
 
 // 	};
 
-// 	this.update = function () {
-
-// 		var targetPosition = new THREE.Vector3();
-
-// 		return function update( delta ) {
-
-// 			if ( this.enabled === false ) return;
-
-// 			if ( this.heightSpeed ) {
-
-// 				var y = THREE.Math.clamp( this.object.position.y, this.heightMin, this.heightMax );
-// 				var heightDelta = y - this.heightMin;
-
-// 				this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
-
-// 			} else {
-
-// 				this.autoSpeedFactor = 0.0;
-
-// 			}
-
-// 			var actualMoveSpeed = delta * this.movementSpeed;
-
-// 			if ( this.moveForward || ( this.autoForward && ! this.moveBackward ) ) this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) );
-// 			if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
-
-// 			if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
-// 			if ( this.moveRight ) this.object.translateX( actualMoveSpeed );
-
-// 			if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
-// 			if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
-
-// 			var actualLookSpeed = delta * this.lookSpeed;
-
-// 			if ( ! this.activeLook ) {
-
-// 				actualLookSpeed = 0;
-
-// 			}
-
-// 			var verticalLookRatio = 1;
-
-// 			if ( this.constrainVertical ) {
-
-// 				verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
-
-// 			}
-
-// 			lon -= this.mouseX * actualLookSpeed;
-// 			if ( this.lookVertical ) lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
-
-// 			lat = Math.max( - 85, Math.min( 85, lat ) );
-
-// 			var phi = THREE.Math.degToRad( 90 - lat );
-// 			var theta = THREE.Math.degToRad( lon );
-
-// 			if ( this.constrainVertical ) {
-
-// 				phi = THREE.Math.mapLinear( phi, 0, Math.PI, this.verticalMin, this.verticalMax );
-
-// 			}
-
-// 			var position = this.object.position;
-
-// 			targetPosition.setFromSphericalCoords( 1, phi, theta ).add( position );
-
-// 			this.object.lookAt( targetPosition );
-
-// 		};
-
-// 	}();
 
 // 	function contextmenu( event ) {
 
